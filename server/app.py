de9205950d726e50eaeba4f2aee785b1e784f3a9
Your Name <your.email@example.com>
2026-01-19 14:49:25 -0500

    first try

--- server/app.py
+++ server/app.py
@@ -6,16 +6,85 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User, UserSchema
 
+user_schema = UserSchema()
+
+
+with app.app_context():
+    db.create_all()
+
+
 class ClearSession(Resource):
 
-    def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+    def get(self):
+        session.pop('page_views', None)
+        session.pop('user_id', None)
+        return '', 204
 
-        return {}, 204
+    def delete(self):
+        session.pop('page_views', None)
+        session.pop('user_id', None)
+        return '', 204
+
+
+class Signup(Resource):
+
+    def post(self):
+        data = request.get_json() or {}
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User(username=username)
+        user.password_hash = password
+
+        db.session.add(user)
+        db.session.commit()
+
+        session['user_id'] = user.id
+        return user_schema.dump(user), 201
+
+
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+        if not user_id:
+            return '', 204
+
+        user = User.query.filter(User.id == user_id).first()
+        if not user:
+            return '', 204
+
+        return user_schema.dump(user), 200
+
+
+class Login(Resource):
+
+    def post(self):
+        data = request.get_json() or {}
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter(User.username == username).first()
+        if not user or not user.authenticate(password):
+            return {}, 401
+
+        session['user_id'] = user.id
+        return user_schema.dump(user), 200
+
+
+class Logout(Resource):
+
+    def delete(self):
+        session.pop('user_id', None)
+        return '', 204
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -13,16 +13,19 @@ class User(db.Model):
     # Build method to protect password_hash property
     @hybrid_property
     def password_hash(self):
-        pass
+        raise Exception('Password hashes may not be viewed.')
 
     # Build method to set password hash property using bcrypt.generate_password_hash()
     @password_hash.setter
     def password_hash(self, password):
-        pass
+        password_bytes = password.encode('utf-8')
+        pw_hash = bcrypt.generate_password_hash(password_bytes).decode('utf-8')
+        self._password_hash = pw_hash
 
     # Build authenticate method that uses bcrypt.check_password_hash()
     def authenticate(self, password):
-        pass
+        password_bytes = password.encode('utf-8')
+        return bcrypt.check_password_hash(self._password_hash, password_bytes)
 
     def __repr__(self):
         return f'User {self.username}, ID: {self.id}'
